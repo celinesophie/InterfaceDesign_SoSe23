943b01a7b15ae87dc868df456465874078c945c7
celinesophie <celine280201@web.de>
2023-06-22 23:08:53 +0200

    red background and new triangles

--- creArt/Canvas.js
+++ creArt/Canvas.js
@@ -3,10 +3,10 @@ var creArtMain;
 (function (creArtMain) {
     window.addEventListener("load", handleLoad);
     let crc2;
-    let imgData;
-    let audioHappy = new Audio("../audio/HeartbreakAnthem.mp3");
-    let audioSad = new Audio("../audio/GoodEnough.mp3");
-    let audioAngry = new Audio("");
+    // let imgData: ImageData;
+    // let audioHappy = new Audio("../audio/HeartbreakAnthem.mp3");
+    // let audioSad = new Audio("../audio/GoodEnough.mp3");
+    // let audioAngry = new Audio("");
     let woosh = new Audio("../sounds/short-woosh.mp3");
     let pencil = new Audio("../sounds/scribble1.mp3");
     let splash = new Audio("../sounds/splash-drop.mp3");
@@ -15,7 +15,7 @@ var creArtMain;
         let canvas = document.querySelector("canvas");
         crc2 = canvas.getContext("2d");
         //get image data
-        imgData = crc2.getImageData(0, 0, canvas.width, canvas.height);
+        // imgData = crc2.getImageData(0, 0, canvas.width, canvas.height);
         // crc2.putImageData(imgData, 0, 0); // das gespeicherte Bild wiederverwenden
         //verschiedene Tasten drücken um Bild zu generieren
         document.addEventListener("keydown", function (event) {
@@ -25,7 +25,7 @@ var creArtMain;
                 // audioHappy.currentTime = 0;
                 // audioSad.pause;
                 // audioSad.currentTime = 0;
-                drawBackgroundHappy();
+                drawBackgroundBlue();
                 // imgData = crc2.getImageData(0, 0, canvas.width, canvas.height);
             }
             if (event.key === "ArrowDown") {
@@ -34,7 +34,16 @@ var creArtMain;
                 // audioHappy.currentTime = 0;
                 // audioSad.pause;
                 // audioSad.currentTime = 0;
-                drawBackgroundSad();
+                drawBackgroundRed();
+                // imgData = crc2.getImageData(0, 0, canvas.width, canvas.height);
+            }
+            if (event.key === "ArrowRight") {
+                splash.play();
+                // audioHappy.pause;
+                // audioHappy.currentTime = 0;
+                // audioSad.pause;
+                // audioSad.currentTime = 0;
+                drawBackgroundYellow();
                 // imgData = crc2.getImageData(0, 0, canvas.width, canvas.height);
             }
             if (event.key === "a") {
@@ -55,7 +64,7 @@ var creArtMain;
         });
     } // handleload
     //FUNKTIONEN
-    function drawBackgroundHappy() {
+    function drawBackgroundBlue() {
         // let videoWaves = document.createElement("video");
         // videoWaves.src = "../videos/waves.mp4"
         // videoWaves.controls = false;
@@ -63,7 +72,7 @@ var creArtMain;
         // videoWaves.width = 1920;
         // document.body.appendChild(videoWaves)
         // Create a linear gradient
-        let gradient = crc2.createLinearGradient(0, 0, 899, 499);
+        let gradient = crc2.createLinearGradient(0, 0, 1920, 1080);
         //gradient stop 1
         let h1 = Math.floor(Math.random() * 60) + 160; //farbe
         let s1 = Math.floor(Math.random() * 40) + 60; //saturation
@@ -83,46 +92,72 @@ var creArtMain;
         gradient.addColorStop(1, "hsla(" + h3 + "," + s3 + "%," + l3 + "%," + a1 + ")");
         // Set the fill style and draw a rectangle
         crc2.fillStyle = gradient;
-        crc2.fillRect(0, 0, 900, 600);
+        crc2.fillRect(0, 0, 1920, 1080);
         //audio
         // audioHappy.play();
     }
-    function drawBackgroundSad() {
+    function drawBackgroundRed() {
         // Create a linear gradient
-        let gradient = crc2.createLinearGradient(0, 0, 899, 499);
+        let gradient = crc2.createLinearGradient(0, 0, 1920, 1080);
         //gradient stop 1
-        let h1 = Math.floor(Math.random() * 241); //farbe
-        let s1 = Math.floor(Math.random() * 20) + 30; //saturation
-        let l1 = Math.floor(Math.random() * 10) + 20; //luminanz
+        let h1 = Math.floor(Math.random() * 31); //farbe
+        let s1 = Math.floor(Math.random() * 20) + 80; //saturation
+        let l1 = Math.floor(Math.random() * 20) + 30; //luminanz
         let a1 = 1; //durchsichtigkeit
         //gradient stop 2
-        let h2 = Math.floor(Math.random() * 241); //farbe
-        let s2 = Math.floor(Math.random() * 20) + 30; //saturation
-        let l2 = Math.floor(Math.random() * 10) + 20; //luminanz
+        let h2 = Math.floor(Math.random() * 31); //farbe
+        let s2 = Math.floor(Math.random() * 20) + 80; //saturation
+        let l2 = Math.floor(Math.random() * 20) + 30; //luminanz
         //gradient stop 3
-        let h3 = Math.floor(Math.random() * 241); //farbe
-        let s3 = Math.floor(Math.random() * 20) + 30; //saturation
-        let l3 = Math.floor(Math.random() * 10) + 20; //luminanz
+        let h3 = Math.floor(Math.random() * 31); //farbe
+        let s3 = Math.floor(Math.random() * 20) + 80; //saturation
+        let l3 = Math.floor(Math.random() * 20) + 30; //luminanz
         // Add three color stops
         gradient.addColorStop(0, "hsla(" + h1 + "," + s1 + "%," + l1 + "%," + a1 + ")");
         gradient.addColorStop(.5, "hsla(" + h2 + "," + s2 + "%," + l2 + "%," + a1 + ")");
         gradient.addColorStop(1, "hsla(" + h3 + "," + s3 + "%," + l3 + "%," + a1 + ")");
         // Set the fill style and draw a rectangle
         crc2.fillStyle = gradient;
-        crc2.fillRect(0, 0, 900, 600);
+        crc2.fillRect(0, 0, 1920, 1080);
         //audio
         // audioSad.play();  
     }
+    function drawBackgroundYellow() {
+        // Create a linear gradient
+        let gradient = crc2.createLinearGradient(0, 0, 1920, 1080);
+        //gradient stop 1
+        let h1 = Math.floor(Math.random() * 21) + 50; //farbe
+        let s1 = Math.floor(Math.random() * 30) + 70; //saturation
+        let l1 = Math.floor(Math.random() * 20) + 30; //luminanz
+        let a1 = 1; //durchsichtigkeit
+        //gradient stop 2
+        let h2 = Math.floor(Math.random() * 21) + 50; //farbe
+        let s2 = Math.floor(Math.random() * 30) + 70; //saturation
+        let l2 = Math.floor(Math.random() * 20) + 30; //luminanz
+        //gradient stop 3
+        let h3 = Math.floor(Math.random() * 21) + 50; //farbe
+        let s3 = Math.floor(Math.random() * 30) + 70; //saturation
+        let l3 = Math.floor(Math.random() * 20) + 30; //luminanz
+        // Add three color stops
+        gradient.addColorStop(0, "hsla(" + h1 + "," + s1 + "%," + l1 + "%," + a1 + ")");
+        gradient.addColorStop(.5, "hsla(" + h2 + "," + s2 + "%," + l2 + "%," + a1 + ")");
+        gradient.addColorStop(1, "hsla(" + h3 + "," + s3 + "%," + l3 + "%," + a1 + ")");
+        // Set the fill style and draw a rectangle
+        crc2.fillStyle = gradient;
+        crc2.fillRect(0, 0, 1920, 1080);
+        //audio
+        // audioHappy.play();
+    }
     function drawRect() {
         //random position as variables
-        let x = Math.random() * 700;
-        let y = Math.random() * 500;
+        let x = Math.random() * 1920;
+        let y = Math.random() * 1080;
         //random number for shape
         let i = Math.random() * 3;
         //draw rects
         if (i < 1) {
             crc2.beginPath();
-            crc2.lineWidth = 2;
+            crc2.lineWidth = 3;
             crc2.strokeStyle = "black";
             crc2.strokeRect(x, y, 250, 180);
             crc2.closePath();
@@ -138,7 +173,7 @@ var creArtMain;
         }
         if (i > 1.5) {
             crc2.beginPath();
-            crc2.lineWidth = 2;
+            crc2.lineWidth = 1;
             crc2.strokeStyle = "black";
             crc2.strokeRect(x, y, 150, 50);
             crc2.closePath();
@@ -147,30 +182,65 @@ var creArtMain;
     }
     function drawCircle() {
         //random position as variables
-        let x = Math.random() * 700;
-        let y = Math.random() * 500;
+        let x = Math.random() * 1920;
+        let y = Math.random() * 1080;
         //random number for radius
-        let r = Math.random() * 50;
+        let r = Math.random() * 70;
         crc2.beginPath();
         crc2.arc(x, y, r, 0, 2 * Math.PI);
-        crc2.lineWidth = 1;
+        if (r >= 25) {
+            crc2.lineWidth = 2;
+        }
+        else {
+            crc2.lineWidth = 1;
+        }
         crc2.strokeStyle = "black";
         crc2.stroke();
         crc2.closePath();
     }
     function drawTriangle() {
         //random position as variables
-        let x = Math.random() * 700;
-        let y = Math.random() * 500;
-        crc2.beginPath();
-        crc2.moveTo(x, y);
-        crc2.lineTo(x + 30, y);
-        crc2.lineTo(x + 15, y - 15);
-        crc2.lineTo(x, y);
-        crc2.lineWidth = 1;
-        crc2.strokeStyle = "black";
-        crc2.stroke();
-        crc2.closePath();
+        let x = Math.random() * 1920;
+        let y = Math.random() * 1080;
+        //random number for different triangles
+        let i = Math.random() * 4;
+        i;
+        if (i >= 0 && i < 2) {
+            console.log(i);
+            crc2.beginPath();
+            crc2.moveTo(x, y);
+            crc2.lineTo(x + 30, y);
+            crc2.lineTo(x + 15, y - 15);
+            crc2.lineTo(x, y);
+            crc2.lineWidth = 1;
+            crc2.strokeStyle = "black";
+            crc2.stroke();
+            crc2.closePath();
+        }
+        else if (i >= 2 && i < 3) {
+            console.log(i);
+            crc2.beginPath();
+            crc2.moveTo(x, y);
+            crc2.lineTo(x + 100, y);
+            crc2.lineTo(x + 50, y - 55);
+            crc2.lineTo(x, y);
+            crc2.lineWidth = 2;
+            crc2.strokeStyle = "black";
+            crc2.stroke();
+            crc2.closePath();
+        }
+        else {
+            console.log(i);
+            crc2.beginPath();
+            crc2.moveTo(x, y);
+            crc2.lineTo(x + 60, y);
+            crc2.lineTo(x + 30, y - 45);
+            crc2.lineTo(x, y);
+            crc2.lineWidth = 2;
+            crc2.strokeStyle = "black";
+            crc2.stroke();
+            crc2.closePath();
+        }
     }
 })(creArtMain || (creArtMain = {})); //namespace
 //# sourceMappingURL=Canvas.js.map
--- creArt/Canvas.ts
+++ creArt/Canvas.ts
@@ -3,15 +3,15 @@ namespace creArtMain {
     window.addEventListener("load", handleLoad);
 
     let crc2: CanvasRenderingContext2D;
-    let imgData: ImageData;
+    // let imgData: ImageData;
 
-    let audioHappy = new Audio("../audio/HeartbreakAnthem.mp3");
-    let audioSad = new Audio("../audio/GoodEnough.mp3");
-    let audioAngry = new Audio("");
+    // let audioHappy = new Audio("../audio/HeartbreakAnthem.mp3");
+    // let audioSad = new Audio("../audio/GoodEnough.mp3");
+    // let audioAngry = new Audio("");
 
-    let woosh = new Audio ("../sounds/short-woosh.mp3")
-    let pencil = new Audio ("../sounds/scribble1.mp3")
-    let splash = new Audio ("../sounds/splash-drop.mp3")
+    let woosh = new Audio("../sounds/short-woosh.mp3")
+    let pencil = new Audio("../sounds/scribble1.mp3")
+    let splash = new Audio("../sounds/splash-drop.mp3")
 
 
     function handleLoad(_event: Event): void {
@@ -20,7 +20,7 @@ namespace creArtMain {
         crc2 = canvas.getContext("2d")!;
 
         //get image data
-        imgData = crc2.getImageData(0, 0, canvas.width, canvas.height);
+        // imgData = crc2.getImageData(0, 0, canvas.width, canvas.height);
         // crc2.putImageData(imgData, 0, 0); // das gespeicherte Bild wiederverwenden
 
         //verschiedene Tasten drücken um Bild zu generieren
@@ -31,7 +31,7 @@ namespace creArtMain {
                 // audioHappy.currentTime = 0;
                 // audioSad.pause;
                 // audioSad.currentTime = 0;
-                drawBackgroundHappy();
+                drawBackgroundBlue();
                 // imgData = crc2.getImageData(0, 0, canvas.width, canvas.height);
             }
             if (event.key === "ArrowDown") {
@@ -40,7 +40,16 @@ namespace creArtMain {
                 // audioHappy.currentTime = 0;
                 // audioSad.pause;
                 // audioSad.currentTime = 0;
-                drawBackgroundSad();
+                drawBackgroundRed();
+                // imgData = crc2.getImageData(0, 0, canvas.width, canvas.height);
+            }
+            if (event.key === "ArrowRight") {
+                splash.play();
+                // audioHappy.pause;
+                // audioHappy.currentTime = 0;
+                // audioSad.pause;
+                // audioSad.currentTime = 0;
+                drawBackgroundYellow();
                 // imgData = crc2.getImageData(0, 0, canvas.width, canvas.height);
             }
             if (event.key === "a") {
@@ -62,7 +71,7 @@ namespace creArtMain {
     } // handleload
 
     //FUNKTIONEN
-    function drawBackgroundHappy(): void {
+    function drawBackgroundBlue(): void {
 
         // let videoWaves = document.createElement("video");
         // videoWaves.src = "../videos/waves.mp4"
@@ -73,7 +82,7 @@ namespace creArtMain {
         // document.body.appendChild(videoWaves)
 
         // Create a linear gradient
-        let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 899, 499);
+        let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 1920, 1080);
 
         //gradient stop 1
         let h1: number = Math.floor(Math.random() * 60) + 160; //farbe
@@ -96,30 +105,30 @@ namespace creArtMain {
 
         // Set the fill style and draw a rectangle
         crc2.fillStyle = gradient;
-        crc2.fillRect(0, 0, 900, 600);
+        crc2.fillRect(0, 0, 1920, 1080);
 
         //audio
         // audioHappy.play();
     }
 
-    function drawBackgroundSad(): void {
+    function drawBackgroundRed(): void {
 
         // Create a linear gradient
-        let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 899, 499);
+        let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 1920, 1080);
 
         //gradient stop 1
-        let h1: number = Math.floor(Math.random() * 241); //farbe
-        let s1: number = Math.floor(Math.random() * 20) + 30; //saturation
-        let l1: number = Math.floor(Math.random() * 10) + 20; //luminanz
+        let h1: number = Math.floor(Math.random() * 31); //farbe
+        let s1: number = Math.floor(Math.random() * 20) + 80; //saturation
+        let l1: number = Math.floor(Math.random() * 20) + 30; //luminanz
         let a1: number = 1; //durchsichtigkeit
         //gradient stop 2
-        let h2: number = Math.floor(Math.random() * 241); //farbe
-        let s2: number = Math.floor(Math.random() * 20) + 30; //saturation
-        let l2: number = Math.floor(Math.random() * 10) + 20; //luminanz
+        let h2: number = Math.floor(Math.random() * 31); //farbe
+        let s2: number = Math.floor(Math.random() * 20) + 80; //saturation
+        let l2: number = Math.floor(Math.random() * 20) + 30; //luminanz
         //gradient stop 3
-        let h3: number = Math.floor(Math.random() * 241); //farbe
-        let s3: number = Math.floor(Math.random() * 20) + 30; //saturation
-        let l3: number = Math.floor(Math.random() * 10) + 20; //luminanz
+        let h3: number = Math.floor(Math.random() * 31); //farbe
+        let s3: number = Math.floor(Math.random() * 20) + 80; //saturation
+        let l3: number = Math.floor(Math.random() * 20) + 30; //luminanz
 
         // Add three color stops
         gradient.addColorStop(0, "hsla(" + h1 + "," + s1 + "%," + l1 + "%," + a1 + ")");
@@ -128,23 +137,54 @@ namespace creArtMain {
 
         // Set the fill style and draw a rectangle
         crc2.fillStyle = gradient;
-        crc2.fillRect(0, 0, 900, 600);
+        crc2.fillRect(0, 0, 1920, 1080);
 
         //audio
         // audioSad.play();  
     }
+    function drawBackgroundYellow(): void {
+
+        // Create a linear gradient
+        let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 1920, 1080);
+
+        //gradient stop 1
+        let h1: number = Math.floor(Math.random() * 21) + 50; //farbe
+        let s1: number = Math.floor(Math.random() * 30) + 70; //saturation
+        let l1: number = Math.floor(Math.random() * 20) + 30; //luminanz
+        let a1: number = 1; //durchsichtigkeit
+        //gradient stop 2
+        let h2: number = Math.floor(Math.random() * 21) + 50; //farbe
+        let s2: number = Math.floor(Math.random() * 30) + 70; //saturation
+        let l2: number = Math.floor(Math.random() * 20) + 30; //luminanz
+        //gradient stop 3
+        let h3: number = Math.floor(Math.random() * 21) + 50; //farbe
+        let s3: number = Math.floor(Math.random() * 30) + 70; //saturation
+        let l3: number = Math.floor(Math.random() * 20) + 30; //luminanz
+
+        // Add three color stops
+        gradient.addColorStop(0, "hsla(" + h1 + "," + s1 + "%," + l1 + "%," + a1 + ")");
+        gradient.addColorStop(.5, "hsla(" + h2 + "," + s2 + "%," + l2 + "%," + a1 + ")");
+        gradient.addColorStop(1, "hsla(" + h3 + "," + s3 + "%," + l3 + "%," + a1 + ")");
+
+        // Set the fill style and draw a rectangle
+        crc2.fillStyle = gradient;
+        crc2.fillRect(0, 0, 1920, 1080);
+
+        //audio
+        // audioHappy.play();
+    }
 
     function drawRect(): void {
         //random position as variables
-        let x: number = Math.random() * 700;
-        let y: number = Math.random() * 500;
+        let x: number = Math.random() * 1920;
+        let y: number = Math.random() * 1080;
         //random number for shape
         let i: number = Math.random() * 3;
-        
+
         //draw rects
         if (i < 1) {
             crc2.beginPath();
-            crc2.lineWidth = 2;
+            crc2.lineWidth = 3;
             crc2.strokeStyle = "black";
             crc2.strokeRect(x, y, 250, 180);
             crc2.closePath();
@@ -160,43 +200,83 @@ namespace creArtMain {
         }
         if (i > 1.5) {
             crc2.beginPath();
-            crc2.lineWidth = 2;
+            crc2.lineWidth = 1;
             crc2.strokeStyle = "black";
             crc2.strokeRect(x, y, 150, 50);
             crc2.closePath();
             console.log(i);
-        }  
-        
+        }
+
     }
 
-    function drawCircle(): void {       
+    function drawCircle(): void {
         //random position as variables
-        let x: number = Math.random() * 700; 
-        let y: number = Math.random() * 500;
+        let x: number = Math.random() * 1920;
+        let y: number = Math.random() * 1080;
         //random number for radius
-        let r: number = Math.random() * 50;
+        let r: number = Math.random() * 70;
 
         crc2.beginPath();
         crc2.arc(x, y, r, 0, 2 * Math.PI);
-        crc2.lineWidth = 1;
+        if (r >= 25) {
+            crc2.lineWidth = 2;
+
+        } else {
+            crc2.lineWidth = 1;
+
+        }
         crc2.strokeStyle = "black";
         crc2.stroke();
         crc2.closePath();
     }
 
     function drawTriangle(): void {
-         //random position as variables
-         let x: number = Math.random() * 700; 
-         let y: number = Math.random() * 500;
+        //random position as variables
+        let x: number = Math.random() * 1920;
+        let y: number = Math.random() * 1080;
 
-         crc2.beginPath();
-         crc2.moveTo(x, y);
-         crc2.lineTo(x + 30, y);
-         crc2.lineTo(x + 15, y - 15);
-         crc2.lineTo(x, y);
-         crc2.lineWidth = 1;
-         crc2.strokeStyle = "black"
-         crc2.stroke();
-         crc2.closePath();
+        //random number for different triangles
+        let i: number = Math.random() * 4;
+
+        i;
+
+        if (i >= 0 && i < 2) {
+            console.log(i);
+            
+            crc2.beginPath();
+            crc2.moveTo(x, y);
+            crc2.lineTo(x + 30, y);
+            crc2.lineTo(x + 15, y - 15);
+            crc2.lineTo(x, y);
+            crc2.lineWidth = 1;
+            crc2.strokeStyle = "black"
+            crc2.stroke();
+            crc2.closePath();
+        }
+        else if (i >= 2 && i < 3) {
+            console.log(i);
+            crc2.beginPath();
+            crc2.moveTo(x, y);
+            crc2.lineTo(x + 100, y);
+            crc2.lineTo(x + 50, y - 55);
+            crc2.lineTo(x, y);
+            crc2.lineWidth = 2;
+            crc2.strokeStyle = "black"
+            crc2.stroke();
+            crc2.closePath();
+        }
+        else {
+            console.log(i);
+            crc2.beginPath();
+            crc2.moveTo(x, y);
+            crc2.lineTo(x + 60, y);
+            crc2.lineTo(x + 30, y - 45);
+            crc2.lineTo(x, y);
+            crc2.lineWidth = 2;
+            crc2.strokeStyle = "black"
+            crc2.stroke();
+            crc2.closePath();
+        }
+
     }
 } //namespace
